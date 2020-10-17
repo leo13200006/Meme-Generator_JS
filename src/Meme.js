912850644055aa8b62dbe8d59fd522ac18f5922a
imrehan <rehanshaikh2156@gmail.com>
2020-10-18 00:28:50 +0530

    top and bottom text added with styles

--- src/Meme.js
+++ src/Meme.js
@@ -29,29 +29,37 @@ class Meme extends Component{
         this.setState({ randomImg: randMemeImg })
     }
 
+    handleTopChange=(event)=> {
+        this.setState({ topText:event.target.value })
+    }
+
+    handleBottomChange=(event)=> {
+        this.setState({ bottomText:event.target.value })
+    }
+
     render() {
         return (
-            <div className="justify-content-center">
-            <Form className="row container justify-content-center">
-            <Form.Group controlId="formBasicEmail" className="col-lg-6">
+            <div className="justify-content-center ">
+            <Form className="row container justify-content-center mb-sm-3 mb-md-6 mt-sm-3 mt-md-6">
+            <Form.Group controlId="basicInputForm" className="col-lg-6">
             <Form.Label>Top Text</Form.Label>
-             <Form.Control type="email" placeholder="Enter text" />
-            <Form.Text className="text-muted">
+             <Form.Control type="text" placeholder="Enter text" value={this.state.topText} onChange={this.handleTopChange}/>
+            <Form.Text className="text-muted" >
               This will be displayed on top side of the meme.
             </Form.Text>
             </Form.Group>
-            <Form.Group controlId="formBasicEmail" className="col-lg-6">
+            <Form.Group controlId="basicInputForm" className="col-lg-6">
             <Form.Label>Bottom Text</Form.Label>
-             <Form.Control type="email" placeholder="Enter text" />
-            <Form.Text className="text-muted">
+             <Form.Control type="text" placeholder="Enter text" value={this.state.bottomText} onChange={this.handleBottomChange}/>
+            <Form.Text className="text-muted" >
             This will be displayed on bottom side of the meme.
             </Form.Text>
             </Form.Group>
-            <Button variant="primary" onClick={this.handleSubmit}>Next meme</Button>
+            <Button variant="primary" onClick={this.handleSubmit}>Next Meme</Button>
             </Form>
 
 
-            <div className="meme">
+            <div className="meme mb-sm-3 mb-md-6">
                     <img src={this.state.randomImg} alt="" className="img-fluid"/>
                     <h2 className="top">{this.state.topText}</h2>
                     <h2 className="bottom">{this.state.bottomText}</h2>
